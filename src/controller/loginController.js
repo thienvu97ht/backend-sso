f3f657e751b49661a5ad63042d6e13f6d9cf64e0
thienvu97ht <thien.vu97ht123@gmail.com>
2023-05-30 16:38:00 +0700

    #22 Customize Axios và Store Token - Lưu Token vào cookies

--- src/controller/loginController.js
+++ src/controller/loginController.js
@@ -1,6 +1,7 @@
 import { v4 as uuidv4 } from "uuid";
 import { createJWT } from "../middleware/JWTAction";
 import loginRegisterService from "../service/loginRegisterService";
+require("dotenv").config();
 
 const getLoginPage = (req, res) => {
   // validate, redis
@@ -11,48 +12,68 @@ const getLoginPage = (req, res) => {
 };
 
 const verifySSOToken = async (req, res) => {
-  const ssoToken = req.body.ssoToken;
+  try {
+    const ssoToken = req.body.ssoToken;
 
-  if (req.user && req.user.code && req.user.code === ssoToken) {
-    const refreshToken = uuidv4();
+    if (req.user && req.user.code && req.user.code === ssoToken) {
+      const refreshToken = uuidv4();
 
-    // update user
-    await loginRegisterService.updateUserRefreshToken(
-      req.user.email,
-      refreshToken
-    );
+      // update user
+      await loginRegisterService.updateUserRefreshToken(
+        req.user.email,
+        refreshToken
+      );
 
-    // create jwt token
-    let payload = {
-      groupWithRoles: req.user.groupWithRoles,
-      email: req.user.email,
-      username: req.user.username,
-    };
-    let token = createJWT(payload);
+      // create jwt token
+      let payload = {
+        groupWithRoles: req.user.groupWithRoles,
+        email: req.user.email,
+        username: req.user.username,
+      };
+      let token = createJWT(payload);
 
-    const resData = {
-      access_token: token,
-      refresh_toke: refreshToken,
-      groupWithRoles: req.user.groupWithRoles,
-      email: req.user.email,
-      username: req.user.username,
-    };
+      // set cookies
+      res.cookie("access_token", token, {
+        maxAge: process.env.MAX_AGE_ACCESS_TOKEN, // 5s
+        httpOnly: true,
+      });
 
-    // destroy session
-    req.session.destroy(function (err) {
-      req.logout();
-    });
+      res.cookie("refresh_toke", refreshToken, {
+        maxAge: process.env.MAX_AGE_REFRESH_TOKEN, // 1h
+        httpOnly: true,
+      });
 
-    return res.status(200).json({
-      EM: "ok",
-      EC: 0,
-      DT: resData,
-    });
-  } else {
-    return res.status(200).json({
-      EM: "ok",
-      EC: 0,
-      DT: "note match",
+      const resData = {
+        access_token: token,
+        refresh_toke: refreshToken,
+        groupWithRoles: req.user.groupWithRoles,
+        email: req.user.email,
+        username: req.user.username,
+      };
+
+      // destroy session
+      req.session.destroy(function (err) {
+        req.logout();
+      });
+
+      return res.status(200).json({
+        EM: "ok",
+        EC: 0,
+        DT: resData,
+      });
+    } else {
+      return res.status(401).json({
+        EM: "Note match ssoToken",
+        EC: 1,
+        DT: "",
+      });
+    }
+  } catch (error) {
+    console.log("🏆 ~ verifySSOToken ~ error:", error);
+    return res.status(500).json({
+      EM: "Something wrong in the server...",
+      EC: -1,
+      DT: "",
     });
   }
 };
